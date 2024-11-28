d5ea69a886401f34adb21f6a53d0823b0e144972
Jose San Leandro Armendáriz <github@acm-sl.org>
2024-11-28 15:15:52 +0100

    Commit created with sync-pythoneda.sh

--- flake.nix
+++ flake.nix
@@ -41,7 +41,7 @@
         "pythoneda-shared-pythonlang-domain";
       inputs.pythoneda-shared-pythonlang-infrastructure.follows =
         "pythoneda-shared-pythonlang-infrastructure";
-      url = "github:pythoneda-realm-rydnr-def/events-infrastructure/0.0.50";
+      url = "github:pythoneda-realm-rydnr-def/events-infrastructure/0.0.51";
     };
     pythoneda-realm-rydnr-realm = {
       inputs.flake-utils.follows = "flake-utils";
@@ -76,7 +76,7 @@
         "pythoneda-shared-pythonlang-domain";
       inputs.pythoneda-shared-pythonlang-infrastructure.follows =
         "pythoneda-shared-pythonlang-infrastructure";
-      url = "github:pythoneda-shared-artifact-def/events-infrastructure/0.0.55";
+      url = "github:pythoneda-shared-artifact-def/events-infrastructure/0.0.56";
     };
     pythoneda-shared-pythonlang-banner = {
       inputs.flake-utils.follows = "flake-utils";
@@ -97,7 +97,7 @@
         "pythoneda-shared-pythonlang-banner";
       inputs.pythoneda-shared-pythonlang-domain.follows =
         "pythoneda-shared-pythonlang-domain";
-      url = "github:pythoneda-shared-pythonlang-def/infrastructure/0.0.65";
+      url = "github:pythoneda-shared-pythonlang-def/infrastructure/0.0.66";
     };
   };
   outputs = inputs:
