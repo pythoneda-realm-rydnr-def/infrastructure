55e215c92e1b43c6cb2c7ac7cd54d1faa4ec2566
Jose San Leandro Armendáriz <github@acm-sl.org>
2025-01-05 18:35:27 +0100

    Commit created with sync-pythoneda.sh

--- flake.nix
+++ flake.nix
@@ -54,7 +54,7 @@
         "pythoneda-shared-pythonlang-banner";
       inputs.pythoneda-shared-pythonlang-domain.follows =
         "pythoneda-shared-pythonlang-domain";
-      url = "github:pythoneda-realm-rydnr-def/realm/0.0.75";
+      url = "github:pythoneda-realm-rydnr-def/realm/0.0.76";
     };
     pythoneda-shared-artifact-events = {
       inputs.flake-utils.follows = "flake-utils";
